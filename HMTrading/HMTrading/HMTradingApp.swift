3e37457e0c026307243bd09636d678795db83f21
kubarlk <kirill@simple-it.pro>
2023-05-17 15:28:40 +0300

    init swiftUI

---
+++ HMTrading/HMTrading/HMTradingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HMTradingApp.swift
+//  HMTrading
+//
+//  Created by Kirill Kubarskiy on 17.05.23.
+//
+
+import SwiftUI
+
+@main
+struct HMTradingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
